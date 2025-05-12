5d4f51bd1b3a239ed26b0f32e33ee50ac83fd4b2
emonkhan500 <khdilo47@gmail.com>
2025-05-12 22:16:11 +0600

    some did on footer

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -13,13 +13,13 @@ const Footer = () => {
 <div
     className="w-full flex items-center justify-center pt-3 flex-col gap-4 pb-16">
     <img src={logo} alt="logo"
-         className="w-[150px]"/>
+         className="w-[70px] md:w-[150px]"/>
 
     <p className="text-[0.9rem] text-center  md:text-lg lg:text-xl text-gray-600">High level
         experience in buying and selling Property with <span className='text-[#3B9DF8] md:text-lg lg:text-2xl'>PROPERTY ZONE</span>, We produced the best services.
     </p>
 
-    <button className="py-3 px-6 rounded-full bg-gradient-to-r from-[#4394e1] to-[#72e4cd] text-white">Contact Us</button>
+    <button className="py-2 md:py-3 px-4 md:px-6 rounded-full bg-gradient-to-r from-[#4394e1] to-[#72e4cd] text-white">Contact Us</button>
 
     <div className="flex gap-[15px] text-black mt-1 md:mb-14 lg:mb-24">
         <a href='https://www.facebook.com/profile.php?id=100025282659899' className="text-[1.3rem] p-1.5 cursor-pointer rounded-full bg-white text-[#424242] shadow-md">
