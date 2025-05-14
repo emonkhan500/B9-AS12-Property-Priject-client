0c64520d0f01dfda488c61147ac61c6aa070fb8d
emonkhan500 <khdilo47@gmail.com>
2025-05-14 11:12:06 +0600

    complete  review

--- src/Components/NewsLetter/Newsletter.jsx
+++ src/Components/NewsLetter/Newsletter.jsx
@@ -21,14 +21,14 @@ const Newsletter = () => {
         setEmail(e.target.value);
       }
   return (
-    <div className=" bg-slate-50 shadow-lg px-2 xl:px-28 flex flex-col md:flex-row justify-center items-center  gap-2 md:gap-5 ">
+    <div className=" bg-slate-50 pt-10 shadow-lg px-2 xl:px-28 flex flex-col md:flex-row justify-center items-center  gap-2 md:gap-5 ">
       <div className="md:w-1/2   md:relative">
         {/* line */}
       <div className="hidden md:flex md:absolute right-5 top-0 h-64 2xl:h-96 w-[4px] md:my-5 lg:my-[40px] xl:my-14 2xl:my-36 bg-gradient-to-r from-[#4394e1] to-[#72e4cd]"></div>
-      <Lottie animationData={newsletter} className="md:pl-36 xl:pl-48 h-[280px] md:h-[300px] lg:h-full" />
+      <Lottie animationData={newsletter} className="md:pl-28 xl:pl-48 h-[280px] md:h-[300px] lg:h-full" />
       </div>
       
-      <div className="md:w-1/2  md:pr-12  xl:pr-40 rounded-sm text-center md:text-left">
+      <div className="md:w-1/2  md:pr-10  xl:pr-40 rounded-sm text-center md:text-left">
         
         <div className="">
         <h1 className="-mt-6 md:-mt-0 text-[28px] md:text-3xl lg::text-5xl font-bold text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text">Subscribe Newsletter</h1>
--- src/Pages/Home/Latest Review/NewLatestReview.jsx
+++ src/Pages/Home/Latest Review/NewLatestReview.jsx
@@ -20,7 +20,7 @@ const NewLatestReview = () => {
   });
   console.log(reviews);
   return (
-    <div className="pt-16 pb-10">
+    <div className="pt-16 px-1 md:px-3">
       <div className="text-center mb-6 md:mb-14">
         <h3 className="text-3xl md:text-4xl font-bold text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text dark:text-white pb-3">
           Latest Review Given By Users
@@ -30,65 +30,79 @@ const NewLatestReview = () => {
         </p>
       </div>
       <Swiper
-        slidesPerView={3}
-        centeredSlides={true}
-        spaceBetween={30}
-        initialSlide={1}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]} // Removed Navigation, added Autoplay
-        className="mySwiper"
-      >
-        {reviews.map((review) => (
-          <SwiperSlide key={review._id}>
-          <div className="w-full h-[450px] mt-8 md:mt-0 px-6 pt-3 bg-gradient-to-r from-[#3B9DF8]/30 to-[#00C6FF]/30 rounded-lg flex flex-col justify-between">
-            {/* Top Quote */}
-            <FaQuoteLeft className="absolute top-2 left-[1%] text-[1.3rem] text-[#727272]" />
-        
-            {/* Top Section (Image + Description) */}
-            <div className="flex flex-col flex-grow overflow-hidden">
+  centeredSlides={true}
+  spaceBetween={30}
+  initialSlide={1}
+  autoplay={{
+    delay: 2500,
+    disableOnInteraction: false,
+  }}
+  breakpoints={{
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  }}
+  modules={[Autoplay]}
+  className="mySwiper"
+>
+  {reviews.map((review) => (
+    <SwiperSlide key={review._id}>
+      <div className="w-full h-[330px] md:h-[330px] lg:h-[450px] mt-8 md:mt-0 px-2 pt-2 bg-gradient-to-r from-[#3B9DF8]/30 to-[#00C6FF]/30 rounded-lg flex flex-col justify-between relative">
+        {/* Top Quote */}
+       
+
+        {/* Top Section (Image + Description) */}
+        <div className="flex flex-col flex-grow overflow-hidden">
+          <img
+            src={review.photo}
+            alt="demo/image"
+            className=" h-[130px] md:h-[120px] lg:h-[200px] w-full object-cover mt-2 rounded-md"
+          />
+          <p className="text-[#424242] text-center mt-3 lg:mt-7 mb-3 overflow-y-auto max-h-[120px] px-2">
+            {review.reviewDes}
+          </p>
+        </div>
+
+        {/* Bottom Section (User Info) */}
+        <div className="flex items-center relative  justify-between pt-3 pb-1   px-3 lg:px-12">
+        <div className="absolute  top-0 left-0 w-full h-[2px]  bg-gradient-to-r from-[#3B9DF8] to-[#00C6FF]"></div>
+          <div className="md:pb-2  flex flex-col justify-center items-center">
+            <div className="p-[2px] rounded-full bg-gradient-to-r from-[#3B9DF8] to-[#00C6FF]">
               <img
-                src={review.photo}
-                alt="demo/image"
-                className="h-[200px] w-full object-cover mt-4 rounded-md"
+                src={review.userImg}
+                alt=""
+                className="w-10 md:w-14 h-10 md:h-14 bg-white rounded-full"
               />
-        
-              <p className="text-[#424242] text-center mt-4 overflow-y-auto max-h-[100px] px-2">
-                {review.reviewDes}
-              </p>
             </div>
-        
-            {/* Bottom Section (User Info) */}
-            <div className="flex items-center justify-between mt-5 pt-2 pb-2">
-              <div className="pb-2 ">
-                <div className="p-[2px] rounded-full bg-gradient-to-r from-[#3B9DF8] to-[#00C6FF] inline-block">
-                  <img
-                    src={review.userImg}
-                    alt=""
-                    className="w-14 h-14 bg-white rounded-full"
-                  />
-                </div>
-                <h2 className="text-[1.2rem] font-[600]">{review.userName}</h2>
-              </div>
-              <div className="pb-4">
-                <p className="text-[1rem] text-[#383737] text-base">
-                  <span>Property:</span> {review.propertyName}
-                </p>
-                <p className="text-[1rem] text-[#383737]">
-                  <span>Time:</span> {review.time}
-                </p>
-              </div>
-            </div>
-        
-            {/* Bottom Quote */}
-            <FaQuoteLeft className="absolute bottom-2 right-[5%] rotate-180 text-[1.3rem] text-[#727272]" />
+            <h2 className="text-base font-normal">{review.userName}</h2>
           </div>
-        </SwiperSlide>
-        
-        ))}
-      </Swiper>
+          <div className="">
+            <p className="text-sm md:text-[1rem] text-[#383737]">
+              <span>Property:</span> {review.propertyName}
+            </p>
+            <p className=" text-sm md:text-[1rem] text-[#383737]">
+              <span>Time:</span> {review.time}
+            </p>
+          </div>
+        </div>
+
+        {/* Bottom Quote */}
+       
+      </div>
+    </SwiperSlide>
+  ))}
+</Swiper>
+
+
     </div>
   );
 };
