a92ecd070eade6f7766ebfa7fdea7fc193c5fe35
emonkhan500 <khdilo47@gmail.com>
2025-05-15 23:56:06 +0600

    complete details page

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -65,9 +65,9 @@ console.log(reviews)
 
 
   return (
-    <div className="lg:pt-16 px-1 md:px-24  mx-auto   py-4 rounded-xl">
+    <div className="lg:pt-16 px-1 md:px-12  mx-auto   py-4 rounded-xl">
       
-        <div className="flex justify-between lg:justify-start gap-0 lg:gap-10  pt-12 md:pt-16  mt-10 text-[#331a15]  z-10">
+        <div className="flex justify-between lg:justify-start gap-0 lg:gap-10  pt-12 md:pt-16  mt-8 text-[#331a15]  z-10">
           <div className=" ml-1 md:ml-4 gap-2 bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4]  text-white max-w-[9rem] hover:cursor-pointer  rounded p-2">
           <Link className='flex justify-center items-center gap-4' to="/allproperty">
             <div>
@@ -102,8 +102,8 @@ console.log(reviews)
         </div>
     
 
-      <div className="-mt-14 md:-mt-14 mx-auto bg-gradient-to-r from-[#3B9DF8]/30 to-[#00C6FF]/30 px-1 md:px-3 pt-10 md:pt-16 rounded-2xl flex flex-col justify-center items-center md:gap-[10%]">
-        {photo && <img className="w-full h-auto md:h-[600px] rounded-md pt-6" src={photo} alt='' />}
+      <div className=" mx-auto  px-1 md:px-3 pt-4 rounded-2xl flex flex-col justify-center items-center md:gap-[10%]">
+        {photo && <img className="w-full h-auto md:h-[600px] rounded-md pt-3" src={photo} alt='' />}
         <div className='flex mt-6 gap-36 lg:gap-48 '>
           {/* <div className="flex items-center justify-between p-3 bg-green-400 py-4 px-10">
             <div className="flex gap-4 items-center space-x-2">
@@ -117,22 +117,22 @@ console.log(reviews)
           
         </div>
         
-        <div className='flex flex-col justify-between md:flex-row md:gap-36 lg:gap-52 mt-4'>
-          <h1 className='text-xl font-semibold'><span className='text-violet-800 text-base mr-1'>Title:</span>{propertyName}</h1>
-          <h1 className='flex items-center justify-center gap-1 text-xl font-semibold'><IoLocationSharp />{location}</h1>
+        <div className='flex  justify-between md:justify-normal gap-7 md:gap-36 lg:gap-52 mb-5'>
+          <h1 className='text-base md:text-2xl font-semibold'><span className=' text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text  text-base  md:text-2xl mr-1'>Title:</span> {propertyName}</h1>
+          <h1 className='flex items-center justify-center gap-1 text-base  md:text-2xl font-semibold'><IoLocationSharp />{location}</h1>
         </div>
         
-        {/* <div className='flex gap-20 mt-4 lg:gap-44 lg:px-4'>
-          <h1 className='text-xl font-semibold'><span className='text-violet-800 text-base mr-1'>Price:</span>${minPrice}-{maxPrice}</h1>
-          <h1 className='flex items-center justify-center gap-1 text-xl text-orange-500 font-semibold mb-4'><MdVerifiedUser />{isVerified === false ? <><RxLapTimer /> Pending</> : isVerified === true ? 'Verified' : 'Rejected'}</h1>
-        </div> */}
+        <div className='flex  justify-between md:justify-normal gap-7 md:gap-36 lg:gap-52 mb-5'>
+          <h1 className='text-base md:text-2xl font-semibold'><span className='text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text  text-base  md:text-2xl mr-1'>Price:</span>${minPrice}-{maxPrice}</h1>
+          <h1 className='flex items-center justify-center gap-1 text-xl text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text font-semibold mb-4'><MdVerifiedUser className='text-blue-600'/>{isVerified === false ? <><RxLapTimer /> Pending</> : isVerified === true ? 'Verified' : 'Rejected'}</h1>
+        </div>
 
-        <h1 className='text-violet-800 text-base mr-1'>Description <span className='text-lg font-semibold text-gray-800'>{des}</span></h1>
+        <h1 className='text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text text-base mr-1'>Description <span className='text-sm md:text-lg font-normal text-gray-600'>{des}</span></h1>
         <div>
         <hr  className='text-blue-700 mt-5 '/>
         <div className="container mx-auto flex flex-col items-center pb-6 mb-4 md:p-10 md:px-12">
        
-		<h1 className="text-4xl font-semibold text-orange-500 leading-none text-center">Review About This Property</h1>
+		<h1 className="text-2xl md:text-4xl font-semibold  text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text leading-none text-center">Review About This Property</h1>
 	</div>
           <div className='grid grid-cols-1 md:grid-cols-2 '>
           {
@@ -140,7 +140,7 @@ console.log(reviews)
           }
           </div>
         </div>
-        <button onClick={() => setIsModalOpen(true)} className='btn btn-warning w-full mt-4'>Add a Review</button>
+        <button onClick={() => setIsModalOpen(true)} className='bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4]  text-white py-2 font-bold rounded w-full mt-4'>Add a Review</button>
       </div>
 
       {/* DaisyUI Modal */}
@@ -156,8 +156,8 @@ console.log(reviews)
                 className="input input-bordered w-full mt-2 mb-4"
               />
               <div className="modal-action">
-                <button className='btn btn-primary'>Submit Review</button>
-                <button type="button" onClick={() => setIsModalOpen(false)} className='btn btn-secondary'>Cancel</button>
+                <button className='px-2 py-2 rounded bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4]  text-white'>Submit Review</button>
+                <button type="button" onClick={() => setIsModalOpen(false)} className='px-2 py-2 rounded bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4]  text-white'>Cancel</button>
               </div>
             </form>
           </div>
--- src/Components/Details/ShowReview.jsx
+++ src/Components/Details/ShowReview.jsx
@@ -7,7 +7,7 @@ const ShowReview = ({review}) => {
         <div>
             <section className="">
 	
-	<div className="container border h-40 overflow-auto  border-red-600 mx-auto   md:px-10 md:pb-10 ">
+	<div className="container border h-auto md:h-40 overflow-auto  border-blue-600 mx-auto   md:px-10 md:pb-10 ">
 		<div className="flex flex-col items-center lg:mx-0">
 			<div className="relative text-center">
 				
@@ -16,7 +16,7 @@ const ShowReview = ({review}) => {
 				
 			</div>
 			<span className="w-12 h-1 rounded-lg dark:bg-violet-600"></span>
-			<p className='text-orange-500'>--{userName || 'Unknown'} </p>
+			<p className='text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text'>--{userName || 'Unknown'} </p>
 		</div>
 		
 	</div>
