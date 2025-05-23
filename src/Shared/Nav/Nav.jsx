8ac461b17655701683706dcf8e7023a6dc2d4abf
emonkhan500 <khdilo47@gmail.com>
2025-05-23 11:31:20 +0600

    toggle menu added

--- src/Shared/Nav/Nav.jsx
+++ src/Shared/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import { FaCartShopping } from "react-icons/fa6";
 import { Link, NavLink } from "react-router-dom";
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
+import { MdClose, MdMenu } from "react-icons/md";
 import logo from "../../assets/download-C1bLvnDk.jpg";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthPorvider";
@@ -9,6 +10,7 @@ import Swal from "sweetalert2";
 
 const Nav = () => {
   const { logOut, user } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleLogout = () => {
     logOut()
@@ -31,6 +33,10 @@ const Nav = () => {
     } else {
       setTheme("light");
     }
+  };
+  
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   const navLinks = (
@@ -107,44 +113,24 @@ const Nav = () => {
   return (
     <div className=" md:px-8 lg:px-16 xl:px-28 navbar fixed z-50 bg-[#10090980] bg-opacity-40 shadow-sm w-full">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div
-            tabIndex={0}
-            role="button"
-            className="text-[#3B9DF8] mr-4 lg:hidden"
+      <button
+          className="lg:hidden text-3xl mr-3 text-blue-500"
+          onClick={toggleMenu}
+        >
+          {isOpen ? <MdClose /> : <MdMenu />}
+        </button>
+        {/* small menu */}
+        {isOpen && (
+          <aside
+            className={`quick fixed top-16 right-0  bg-black bg-opacity-80 z-20 shadow-lg   w-[200px] md:w-[300px] transition-transform duration-300 ${
+              isOpen ? "translate-x-0" : "translate-x-full"
+            }`}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-9 w-9  "
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-black bg-opacity-80 text-left rounded-box z-1 mt-3 w-64 p-2 shadow"
-          >
-            {/* <li>
-              <a>Homepage</a>
-            </li>
-            <li>
-              <a>Portfolio</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li> */}
-
-            {navLinks}
-          </ul>
-        </div>
+            <ul className="gap-[20px] text-[1rem] flex flex-col mt-5">
+              {navLinks}
+            </ul>
+          </aside>
+        )}
 
         <div className="text-white flex gap-2">
           <img className="h-9 w-9 rounded-lg" src={logo} alt="" />
@@ -211,46 +197,7 @@ const Nav = () => {
           </div>
         )}
       </div>
-      <div className="ml-4">
-        {/* <label className="cursor-pointer grid place-items-center">
-    <input 
-      onChange={handleToggle} 
-      type="checkbox" 
-      className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" 
-    />
-    
-    <svg 
-      className="col-start-1 row-start-1 stroke-base-100 fill-base-100" 
-      xmlns="http://www.w3.org/2000/svg" 
-      width="8" 
-      height="24" 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="5" />
-      <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-    </svg>
-
-    <svg 
-      className="col-start-2 row-start-1 stroke-base-100 fill-base-100" 
-      xmlns="http://www.w3.org/2000/svg" 
-      width="8" 
-      height="24" 
-      viewBox="0 0 24 24" 
-      fill="none" 
-      stroke="currentColor" 
-      strokeWidth="2" 
-      strokeLinecap="round" 
-      strokeLinejoin="round"
-    >
-      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
-    </svg>
-  </label> */}
-      </div>
+      
 
       <div className="ml-4"></div>
 
