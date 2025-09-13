33de73fea85dd3b9c9b4eecc6407eb337ffe578f
emonkhan500 <khdilo47@gmail.com>
2025-09-13 21:01:33 +0600

    issues fix

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -10,113 +10,179 @@ import { IoMdHome } from "react-icons/io";
 import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../Components/useAdmin";
 import useAgent from "../Components/useAgent";
-
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthPorvider";
 
 const Dashboard = () => {
-    const [isAdmin]=useAdmin()
-    const[isAgent]=useAgent()
-    // console.log(isAdmin)
-    // const isAgent = false
-    // const isAdmin = true
-   
-    return (
-        <div className="flex flex-col lg:flex-row min-h-screen ">
-        <div className="  w-auto lg:w-64 xl:w-80 h-full lg:min-h-screen pt-10  lg:pt-20 border-e-2">
-          
-          <ul className="menu space-y-2 mx-5">
-            {isAdmin ? 
-              <>
+  const [isAdmin] = useAdmin();
+  const [isAgent] = useAgent();
+  // console.log(isAdmin)
+  // const isAgent = false
+  // const isAdmin = true
+  const { loading } = useContext(AuthContext);
+  if(loading){
+    return <div className="w-24 h-24 mx-auto mt-10 border-4 border-dashed  rounded-full animate-spin dark:border-violet-600"></div>
+  }
+
+  return (
+    <div className="flex flex-col lg:flex-row min-h-screen ">
+      <div className="  w-auto lg:w-64 xl:w-80 h-full lg:min-h-screen pt-10  lg:pt-20 border-e-2">
+        <ul className="menu space-y-2 mx-5">
+          {isAdmin ? (
+            <>
               <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/adminprofile'><IoMdHome /> Admin Profile</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/managepropertybyadmin'><HiOutlineHomeModern /> Manage Properties</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/manageusersbyadmin'><FaUsers /> Manage Users</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/managereviewsbyadmin'><GoCodeReview /> Manage reviews</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/advertise'><FcAdvertising /> Advertise Property</NavLink>
-      </li>
-      
-              </>
-             : isAgent ?
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/adminprofile"
+                >
+                  <IoMdHome /> Admin Profile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/managepropertybyadmin"
+                >
+                  <HiOutlineHomeModern /> Manage Properties
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/manageusersbyadmin"
+                >
+                  <FaUsers /> Manage Users
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/managereviewsbyadmin"
+                >
+                  <GoCodeReview /> Manage reviews
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/advertise"
+                >
+                  <FcAdvertising /> Advertise Property
+                </NavLink>
+              </li>
+            </>
+          ) : isAgent ? (
+            <>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/agentprofile"
+                >
+                  <IoMdHome /> Agent Profile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/addproperty"
+                >
+                  <HiOutlineHomeModern /> Add Property
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/myaddedproperty"
+                >
+                  <HiOutlineHomeModern /> My added properties
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/soldproperty"
+                >
+                  <HiOutlineHomeModern /> My sold properties
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/requestedproperty"
+                >
+                  <HiOutlineHomeModern /> Requested properties
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/userprofile"
+                >
+                  <IoMdHome /> My Profile
+                </NavLink>
+              </li>
 
-             <>
-
-<li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/agentprofile'><IoMdHome /> Agent Profile</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/addproperty'><HiOutlineHomeModern /> Add Property</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/myaddedproperty'><HiOutlineHomeModern /> My added properties</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/soldproperty'><HiOutlineHomeModern /> My sold properties</NavLink>
-      </li>
-      <li>
-      <NavLink className='text-lg flex justify-center lg:justify-normal items-center ' to='/dashboard/requestedproperty'><HiOutlineHomeModern /> Requested properties</NavLink>
-      </li>
-             </> 
-             :
-             <>
-                <li>
-                  <NavLink className="text-lg flex justify-center lg:justify-normal items-center " to="/dashboard/userprofile">
-                    <IoMdHome /> My Profile
-                  </NavLink>
-                </li>
-                
-              
-                <li>
-                  <NavLink className="text-lg flex justify-center lg:justify-normal items-center " to="/dashboard/userreview">
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/userreview"
+                >
                   <GoCodeReview /> My Reviews
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className="text-lg flex justify-center lg:justify-normal items-center " to="/dashboard/propertybought">
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/propertybought"
+                >
                   <HiOutlineHomeModern /> Property Bought
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className="text-lg flex justify-center lg:justify-normal items-center " to="/dashboard/wishlist">
-                    <FaBookMedical /> WishList
-                  </NavLink>
-                </li>
-              </>
-            }
-            {/* divider */}
-            <div className="divider"></div>
-  
-            <li>
-              <NavLink className="text-lg flex justify-center lg:justify-normal items-center " to="/">
-                <IoMdHome /> HOME
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className="text-lg flex justify-center lg:justify-normal items-center " to="/allproperty">
-                <IoMenu /> ALL PROPERTY
-              </NavLink>
-            </li>
-            
-            
-          </ul>
-        </div>
-  
-        <div className="flex-1 ">
-          <div className="text-center mt-6 mb-10 lg:mb-16 lg:mt-14 text-2xl md:text-4xl  lg:text-5xl xl:text-7xl text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text">
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-lg flex justify-center lg:justify-normal items-center "
+                  to="/dashboard/wishlist"
+                >
+                  <FaBookMedical /> WishList
+                </NavLink>
+              </li>
+            </>
+          )}
+          {/* divider */}
+          <div className="divider"></div>
+
+          <li>
+            <NavLink
+              className="text-lg flex justify-center lg:justify-normal items-center "
+              to="/"
+            >
+              <IoMdHome /> HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="text-lg flex justify-center lg:justify-normal items-center "
+              to="/allproperty"
+            >
+              <IoMenu /> ALL PROPERTY
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex-1 ">
+        <div className="text-center mt-6 mb-10 lg:mb-16 lg:mt-14 text-2xl md:text-4xl  lg:text-5xl xl:text-7xl text-transparent bg-gradient-to-r from-[#3B9DF8] to-[#3BE8C4] bg-clip-text">
           <h1>-Welcome to Dashboard-</h1>
-          </div>
-          <div>
+        </div>
+        <div>
           <Outlet></Outlet>
-          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Dashboard;
